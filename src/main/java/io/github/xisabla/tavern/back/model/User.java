24ccedaffe5e2b0da31da9007e604e2f3fe02233
Gautier Miquet <xisabla.dev@gmail.com>
2024-09-18 17:25:45 +0200

    feat(models): Add User model

---
+++ src/main/java/io/github/xisabla/tavern/back/model/User.java
@@ -0,0 +1,70 @@
+package io.github.xisabla.tavern.back.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Application user model.
+ */
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true)
+    @Size(min = 3, max = 30)
+    private String username;
+
+    @Column(unique = true)
+    @Size(min = 3, max = 128)
+    private String email;
+
+    @JsonIgnore
+    @Size(min = 3, max = 128)
+    private String password;
+
+    @Builder.Default
+    private String role = "USER";
+
+    @Builder.Default
+    private boolean enabled = true;
+
+    @Builder.Default
+    private boolean locked = false;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+}
