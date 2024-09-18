cf971fc434c45993e9311afccaa7cb613ff831bc
Gautier Miquet <xisabla.dev@gmail.com>
2024-09-18 15:26:28 +0200

    chore(core): Initialize spring project

---
+++ src/main/java/io/github/xisabla/tavern/back/BackApplication.java
@@ -0,0 +1,13 @@
+package io.github.xisabla.tavern.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackApplication.class, args);
+    }
+
+}
