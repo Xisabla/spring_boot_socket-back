4dc8fd546b43b52d5f68fcc7c516e94dde5e79fb
Gautier Miquet <xisabla.dev@gmail.com>
2024-09-24 16:00:07 +0200

    feat(models): Add message model

---
+++ src/main/java/io/github/xisabla/tavern/back/model/Message.java
@@ -0,0 +1,46 @@
+package io.github.xisabla.tavern.back.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Chat message model.
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Size(max = 4096)
+    private String content;
+
+    @ManyToOne
+    private User author;
+
+    @ManyToOne
+    private Channel channel;
+
+    @Builder.Default
+    private boolean deleted = false;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+
+}
