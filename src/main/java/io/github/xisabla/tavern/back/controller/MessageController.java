a4f04f2bd643f735a19b858c31f40d42352ee8df
Gautier Miquet <xisabla.dev@gmail.com>
2024-09-24 16:01:53 +0200

    feat(message): Add MessageController

---
+++ src/main/java/io/github/xisabla/tavern/back/controller/MessageController.java
@@ -0,0 +1,32 @@
+package io.github.xisabla.tavern.back.controller;
+
+import io.github.xisabla.tavern.back.dto.MessageCreateDto;
+import io.github.xisabla.tavern.back.model.Message;
+import io.github.xisabla.tavern.back.model.User;
+import io.github.xisabla.tavern.back.service.MessageService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for managing messages.
+ */
+@RestController
+@RequestMapping("/messages")
+@RequiredArgsConstructor
+public class MessageController {
+    private final MessageService messageService;
+
+    @PostMapping
+    public ResponseEntity<Message> createMessage(@RequestBody @Valid MessageCreateDto messageCreateDto) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Message message = messageService.createMessage(messageCreateDto, user);
+
+        return ResponseEntity.ok(message);
+    }
+}
